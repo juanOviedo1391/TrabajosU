bc0de9c4f1437fcbd96dfcf5ab26971119ca7613
Tu Nombre <tu_email@example.com>
2025-02-23 11:00:27 -0500

    Primer commit

---
+++ app/src/main/java/com/example/tallern2/splashActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tallern2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class splashActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
---
+++ app/src/main/java/com/example/tallern2/splashJava.java
@@ -0,0 +1,13 @@
+package com.example.tallern2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashJava extends AppCompatActivity {
+
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
